a88c6204772956a11e3279fdbe692af38db638a7
David Chang <zeta11235813@gmail.com>
2015-08-26 16:59:45 +0800

    Make the complex example more fun
    * Add opal-browser
    * Hook on document.ready

--- examples/complex/main.rb
+++ examples/complex/main.rb
@@ -1,4 +1,21 @@
 require "opal"
+require "browser"
 require "react"
 
-React.render(React.create_element('h1'){ "Hello World!" }, `document.body`)
+class HelloFromReact
+  include React::Component
+  
+  def render
+    h1 { "Hello from React!" }
+  end
+end
+
+$document.ready do
+  DOM {
+    div.info {
+      span.red "I'm all cooked up."
+    }
+  }.append_to($document['#opal-browser'])
+  
+  React.render(React.create_element(HelloFromReact), $document['#react'].to_n)
+end
